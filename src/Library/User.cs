60de49d50b57957ad72f77a43055c0f0f3132573
Macarenapineiro1 <macarena.pineiro@correo.ucu.edu.uy>
2024-10-15 11:47:20 -0300

    User

---
+++ src/Library/User.cs
@@ -0,0 +1,17 @@
+﻿namespace Library;
+
+public abstract class User
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public int Id { get; set; }
+    public int Rating { get; set; }
+
+    protected User (string name, string surname, int id, int rating)
+    {
+        this.Name = name;
+        this.Surname = surname;
+        this.Id = id;
+        this.Rating = rating;
+    }
+}
