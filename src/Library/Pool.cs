a58afa5ae521acc55a7ad54fa817ff08fc7e160f
Macarenapineiro1 <macarena.pineiro@correo.ucu.edu.uy>
2024-10-15 12:06:01 -0300

    Pool

---
+++ src/Library/Pool.cs
@@ -0,0 +1,10 @@
+﻿namespace Library;
+
+public class Pool : Driver
+{
+    public int Capacity { get; set; }
+    public Pool(string name, string surname, int id, int rating, string vehicle, int capacity) : base(name, surname, id, rating, vehicle)
+    {
+        this.Capacity = capacity;
+    }
+}
